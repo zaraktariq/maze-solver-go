cb4181c0186458f490cca78528d9c865d24d0f73
zarak <ztariq@luc.edu>
2024-04-17 12:37:04 -0500

    inital print of maze

--- main.go
+++ main.go
@@ -1,12 +1,36 @@
 package main
 
-import _ "fmt"
+import (
+	"fmt"
+	_ "fmt"
+)
 
 type Point struct {
 	x int
 	y int
 }
 
-func main() {
+type Maze [][]int
 
+func (maze Maze) PrintMaze() {
+	for _, row := range maze {
+		for _, cell := range row {
+			fmt.Print(cell, " ")
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	// Initialize a sample maze
+	maze := Maze{
+		{0, 1, 0, 0, 0},
+		{0, 1, 0, 1, 0},
+		{0, 0, 0, 1, 0},
+		{1, 1, 1, 1, 0},
+		{0, 0, 0, 0, 0},
+	}
+
+	// Print the maze
+	maze.PrintMaze()
 }
--- translations.md
+++ translations.md
@@ -1,9 +1,11 @@
+#### The code for the struct in Go:
 ```go
 type Point struct {
     x int
     y int
 }
 ```
+#### Is equivalent to this code in Python:
 ```python
 class Point:
 def __init__(self, x, y):
