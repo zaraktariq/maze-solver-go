bde2b9a84698f3ffb5891d4ac96294c12dd8be02
zarak <ztariq@luc.edu>
2024-04-18 14:41:34 -0500

    implement a way for the algo to "understand" its current position in the maze

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	_ "fmt"
 )
 
 type Point struct {
@@ -13,12 +12,29 @@ type Point struct {
 type Maze [][]int
 
 func (maze Maze) PrintMaze() {
+	// iterate iver rows
 	for _, row := range maze {
+		// iterate over cells in current row
 		for _, cell := range row {
+			// print
 			fmt.Print(cell, " ")
 		}
 		fmt.Println()
 	}
+}
+
+func isValidPt(p Point, maze Maze) int {
+	// Check if the point is within the maze
+	if p.x < 0 || p.x > len(maze) ||
+		p.y < 0 || p.y > len(maze[0]) {
+		return -1
+	}
+	// Check if the current point is not a wall
+	if maze[p.y][p.x] == 1 {
+		return -1
+	}
+	// Otherwise, return the value of the cell at the given point
+	return maze[p.y][p.x]
 }
 
 func main() {
--- translations.md
+++ translations.md
@@ -12,3 +12,17 @@ def __init__(self, x, y):
     self.x = x
     self.y = y
 ```
+------
+- := is used for short variable declaration and initialization.
+- = is used for variable assignment.
+------
+#### This is how one prints in Go
+```go
+fmt.Print(cell, " ")
+```
+
+#### In comparison to our familiar python
+```go
+print(cell, end=" ")
+```
+----
